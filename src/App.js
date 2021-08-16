83ef298e47efabada6645b475a2ed5a60ec92b6b
Webdornald <webdornald@gmail.com>
2021-08-16 18:50:26 +0900

    making base movie app structure done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import React from "react";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log('hello');
-    }
-
     state = {
-        count: 0,
+        isLoading: true,
+        movies: [],
     };
-
-    add = () => {
-        this.setState(current => ({
-            count: current.count + 1,
-        }));
-    };
-
-    minus = () => {
-        this.setState(current => ({
-            count: current.count -1,
-        }));
-    }
 
     componentDidMount() {
-        console.log('component rendered');
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('I just updated');
-    }
-
-    componentWillUnmount() {
-        console.log('Goodbye, cruel world');
+        setTimeout(() => {
+            // 영화 데이터 로딩!
+            this.setState({isLoading: false});
+        }, 6000);
     }
 
     render() {
-        console.log("I'm rendering");
-    return (
-        <div>
-            <h1>The number is: {this.state.count}</h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
-        </div>
-    )};
+        const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
+    }
 }
-
 
 export default App;
