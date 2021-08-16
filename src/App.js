2a1106f24d87c7823b54aeb11776d14ea30a524d
Webdornald <webdornald@gmail.com>
2021-08-16 18:40:47 +0900

    component lifecycle practice

--- src/App.js
+++ src/App.js
@@ -1,63 +1,49 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}) {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('hello');
+    }
+
+    state = {
+        count: 0,
+    };
+
+    add = () => {
+        this.setState(current => ({
+            count: current.count + 1,
+        }));
+    };
+
+    minus = () => {
+        this.setState(current => ({
+            count: current.count -1,
+        }));
+    }
+
+    componentDidMount() {
+        console.log('component rendered');
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('I just updated');
+    }
+
+    componentWillUnmount() {
+        console.log('Goodbye, cruel world');
+    }
+
+    render() {
+        console.log("I'm rendering");
     return (
         <div>
-            <h2>I like {name}</h2>;
-            <h4>{rating}/5.0</h4>
-            <img src={picture} alt={name} />
+            <h1>The number is: {this.state.count}</h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
         </div>
-    );
+    )};
 }
 
-const foodILike = [
-    {
-        id: 1,
-        name: 'Kimchi',
-        image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-        rating: 5,
-    },
-    {
-        id: 2,
-        name: 'Samgyeopsal',
-        image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-        rating: 4.5,
-    },
-    {
-        id: 3,
-        name: 'Bibimbap',
-        image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-        rating: 3.2,
-    },
-    {
-        id: 4,
-        name: 'Doncasu',
-        image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-        rating: 3.9,
-    },
-    {
-        id: 5,
-        name: 'Kimbap',
-        image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
-        rating: 2.4,
-    },
-];
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(dish => (
-                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-            ))}
-        </div>
-    );
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number,
-}
 
 export default App;
